c1e65f1131f219c697c0dd6db443f74b7cba0dd6
Thomas Lauerman <tom@lauerman.dev>
2024-07-03 21:36:15 -0700

    show a SwiftUI view solution

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-xcrun --sdk iphonesimulator swiftc \
+xcrun --sdk iphonesimulator swiftc -parse-as-library \
   -target arm64-apple-ios17.4-simulator \
   -o handmade-ios.app/handmade-ios \
   main.swift
