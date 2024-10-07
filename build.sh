362a24cd71dbb55272b9bb1b182446036fd3ce88
Thomas Lauerman <tom@lauerman.dev>
2024-10-06 18:37:14 -0700

    part 2 blog post solution

--- build.sh
+++ build.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
-set -e
+set -ex
 
-xcrun -sdk iphonesimulator clang \
-    -o handmade-ios.app/handmade-ios \
-    main.c
+xcrun -sdk iphoneos clang \
+  -o handmade-ios.app/handmade-ios \
+  main.c
+
+# codeign our app and executable
+certificate_name="<YOUR-CERTIFICATE-NAME>"
+codesign -s $certificate_name --entitlements handmade-ios.app/Entitlements.plist -f handmade-ios.app
+codesign -s $certificate_name --entitlements handmade-ios.app/Entitlements.plist -f handmade-ios.app/handmade-ios
+
+# package the app into .ipa
+rm -rf Payload/ handmade-ios.ipa/
+mkdir Payload/
+cp -r handmade-ios.app Payload/
+zip -0yr handmade-ios.ipa Payload/
+rm -rf Payload/
